4167c9674dd7279493e132bf3c3b948850a290e8
Kostas <weedywookie@gmail.com>
2020-09-27 01:19:05 +0200

    CHANGED: stomp method to kill enemies, die method

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,5 +1,13 @@
 extends Actor
 
+export var stomp_impulse: = 1000.0
+
+func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+	velocity = calc_stomp_velocity(velocity, stomp_impulse)
+	
+func _on_EnemyDetector_body_entered(body: PhysicsBody2D) -> void:
+	queue_free()
+	
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and velocity.y < 0.0
 	var direction: =  get_direction()
@@ -25,3 +33,12 @@ func calc_move_velocity(
 	if is_jump_interrupted:
 		out.y = 0.0
 	return out
+
+
+func calc_stomp_velocity(velocity_in: Vector2, impulse: float) -> Vector2:
+	var velocity_out: = velocity_in 
+	velocity_out.y = -impulse
+	return velocity_out
+
+
+
